bb1cee51da5ee496d95ffbcfe74145fd2d89615e
WoWxiaoyao <37071529+WoWxiaoyao@users.noreply.github.com>
2020-02-16 17:31:39 +0800

    增加了一个Task修复刷新BUG

--- src/zbv5/cn/XiaoSign/Main.java
+++ src/zbv5/cn/XiaoSign/Main.java
@@ -1,5 +1,6 @@
 package zbv5.cn.XiaoSign;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import zbv5.cn.XiaoSign.Command.MainCommand;
 import zbv5.cn.XiaoSign.Listener.PlayerListener;
@@ -32,6 +33,19 @@ public class Main extends JavaPlugin
         DateUtil.getWeekDate();
         getServer().getPluginManager().registerEvents(new PlayerListener(), this);
         getCommand("XiaoSign").setExecutor(new MainCommand());
+
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new Runnable()
+        {
+            public void run()
+            {
+                if(DateUtil.getDate("HH:mm:ss").equals("00:00:00"))
+                {
+                    DateUtil.getWeekDate();
+                }
+            }
+        }, 20L, 20L);
+
+
         Util.Print("&a插件加载完成");
     }
     @Override
